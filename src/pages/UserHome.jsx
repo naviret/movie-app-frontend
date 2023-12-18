a4929d73ecbefa529dc76a18d089db513ea129cc
naviret <111366578+naviret@users.noreply.github.com>
2023-12-18 04:28:15 -0500

    user login page

---
+++ src/pages/UserHome.jsx
@@ -0,0 +1,13 @@
+import react, { useEffect, useState } from 'react';
+import Welcome from '../components/main/Welcome.jsx'
+
+function UserHome (){
+
+	return (
+		<div>
+            <Welcome/>
+		</div>
+	)
+}
+
+export default UserHome;
