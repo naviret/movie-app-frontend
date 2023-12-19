9bcb46f72d36d296b50f3ff695b1251410f03c15
naviret <111366578+naviret@users.noreply.github.com>
2023-12-19 14:45:37 -0500

    removed unecessary comments

--- src/App.js
+++ src/App.js
@@ -17,45 +17,4 @@ function App() {
 	)
 }
 
-// function App() {
-// 	const [backendData, setBackendData] = useState([{}]);
-
-// 	useEffect(() => {
-// 		fetch('/api')
-// 			.then((response) => response.json())
-// 			.then((data) => {
-// 				setBackendData(data);
-// 			});
-// 	}, []);
-
-// 	// useEffect performs side effects on functional components
-// 	// sideeffects are not directly involved with rendering some UI,
-// 	// but are needed for the component to accomplish its goals
-// 	// used to manage state and lifecycle methods --> basically replace classes
-// 	// useEffect takes two arguments: 1) function containing code for side effects
-// 	// and 2) list of dependencies (if empty, only runs when the component mounts and
-// 	// not on subsequent renders)
-// 	// useEffect hook can be used to fetch data from some server
-// 	// fetch returns a Promise, which is an initiation of a network request
-// 	// this means the fetch is asynchronous, and thus returns a Promise for
-// 	// completion of some request, AT whcih point a Response is ent
-// 	// fetch is usually chained with .then method which activates once
-// 	// a promise is fullfilled
-// 	// .then is a callback method does something once a Response is received
-// 	// using .json method to parse response as JSON
-// 	// .then is used again to update state
-// 	// .then can return another Promise or a value, chained .thens will behave
-// 	// the same on a Promise
-
-// 	return (
-// 		<div>
-// 			{(typeof backendData.users === 'undefined') ? (
-// 				<p>Loading...</p>
-// 			) : (
-// 				backendData.users.map((user, i) => <p key={i}>{user}</p>)
-// 			)}
-// 		</div>
-// 	);
-// }
-
 export default App;
