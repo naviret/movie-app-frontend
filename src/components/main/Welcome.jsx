9fde7d692a6e270672bfe76581b5802b3de887d5
naviret <111366578+naviret@users.noreply.github.com>
2023-12-19 01:27:04 -0500

    completed welcome component

--- src/components/main/Welcome.jsx
+++ src/components/main/Welcome.jsx
@@ -2,15 +2,26 @@ import react, { useEffect, useState } from 'react';
 import './Welcome.css';
 
 
-function Welcome (){
-
-    const name = "ivancito";
+function Welcome (props){
 
 	return (
 		<div className='welcome-container'>
             <div className='welcome-header'>
                 <div className='welcome-text'>
-                    Welcome back {name}!
+                    Welcome back <span>{props.name}</span>
+                </div>
+            </div>
+            <div className='movie-request'>
+                <div className='movie-request-header'>
+                    What <span>genre</span> are you looking for today?
+                </div>
+                <div className='movie-request-input'>
+                    {/* <img src='' alt=''/> */}
+                    <input 
+                        type='text' 
+                        placeholder='Genre'
+                        // onChange={(e) => setEmail(e.target.value.trim())}
+                    />
                 </div>
             </div>
 		</div>
