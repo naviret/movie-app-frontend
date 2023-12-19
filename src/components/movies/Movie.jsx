6e446ac16d59f9d002cfce6ba6da6f3cc0430648
naviret <111366578+naviret@users.noreply.github.com>
2023-12-19 10:22:04 -0500

    movie components for user home page

---
+++ src/components/movies/Movie.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+import './Movies.css';
+
+const basePosterUrl = 'https://image.tmdb.org/t/p/w300'; 
+
+function Movie (props){
+
+    const path = `${basePosterUrl}${props.movie.poster_path}`
+
+    const handleClick = () => {
+        props.saveMovie(props.movie.title);
+    }
+
+	return (
+		<div>
+            <img className='poster' src={path} alt='movie' onClick={handleClick}></img>
+		</div>
+	)
+}
+
+export default Movie;
---
+++ src/components/movies/MoviesList.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+import Movie from './Movie';
+import './Movies.css';
+
+
+function MoviesList (props){
+
+	return (
+		<div className='poster-container'> 
+            {props.movies.map((movie, index) => (<Movie key={index} movie={movie} saveMovie={props.saveMovie}/>))}
+        </div> 
+	)
+}
+
+export default MoviesList;
