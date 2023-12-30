49bc192e0bebb2e5e11fb2838e6c08dd44271ea1
naviret <111366578+naviret@users.noreply.github.com>
2023-12-30 18:30:24 -0500

    fixed type in home page

--- src/pages/WebHomePage.jsx
+++ src/pages/WebHomePage.jsx
@@ -1,38 +1,34 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Pages.css'
+import './Pages.css';
 
-function Home (){
-	const navigate = useNavigate()
+function Home() {
+	const navigate = useNavigate();
 
 	const handleClick = () => {
-		navigate('/authenticate')
-	}
+		navigate('/authenticate');
+	};
 
 	return (
 		<div className='web-home-container'>
-            <div className='web-home-header'>
-				Movie Recommendation App
-			</div>
+			<div className='web-home-header'>Movie Recommendation App</div>
 			<p className='web-home-p'>
-				This is a movie recommendation app built using the MERN 
-				(MongoDB, Express, React, Node) framework. This app 
-				contains a user registration/login form which stores 
-				users in a MongoDB database. Note that usernames and 
-				emails cannot be duplicated in the database. Upon login, 
-				the user is presented with a personalized home page
-				with the option to query movie recommendations from
-				<b>The Movie Database (TMDB) </b> based on genre.
-
-				In future work, I'd like to improve the recommendation
-				by using ML recommendation systems like content-based 
-				and collaborative filtering.
+				This is a movie recommendation app built using the MERN
+				(MongoDB, Express, React, Node) framework. This app contains a
+				user registration/login form which stores users in a MongoDB
+				database. Note that usernames and emails cannot be duplicated in
+				the database. Upon login, the user is presented with a
+				personalized home page with the option to query movie
+				recommendations from <b>The Movie Database (TMDB) </b> based on
+				genre. In future work, I'd like to improve the recommendation by
+				using ML recommendation systems like content-based and
+				collaborative filtering.
 			</p>
 			<div className='web-home-authenticate' onClick={handleClick}>
 				Login
 			</div>
 		</div>
-	)
+	);
 }
 
 export default Home;
